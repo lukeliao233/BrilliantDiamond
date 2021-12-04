e1ff4dba4758abd851e33aedd0f4845d46615585
Madao <83897059+lukeliao233@users.noreply.github.com>
2021-12-04 00:30:18 -0500

    testing

--- Testing.r
+++ Testing.r
@@ -1,2 +1,17 @@
-library(MASS)
-names(Boston)
+library(ISLR)
+Diamond.file = read.csv("diamonds_dataset.csv",header=T,na.string="?")
+fix(Diamond.file)
+names(Diamond.file)
+dim(Diamond.file)
+Diamond.file=na.omit(Diamond.file)
+dim(Diamond.file) #Nothing was removed so no missing observations 
+attach(Diamond.file)
+plot(price, carat)
+Diamond.file$url <- NULL  #removed url column
+summary(Diamond.file)
+
+#Simple linear regression model
+lm.fit=lm(price~carat)
+lm.fit
+summary(lm.fit)
+names(lm.fit)
